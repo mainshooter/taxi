9ebb97197171d160b90d43dd3116896a09d09093
Peter 4A5A <498883@edu.rocmn.nl>
2017-04-10 16:34:23 +0200

    Added taxi files

---
+++ main.js
@@ -0,0 +1,113 @@
+function getKilometers() {
+  kilometers = $("kilometers").value;
+  return(kilometers);
+}
+function weekendTime() {
+  // This function runs when the checkIfDayIsWeekend is runt
+  // And the weekend if true
+  var startTime = $("begin_tijd").value;
+
+  startTime = startTime.replace(":", "");
+  if (startTime >= 700 && startTime <= 2200) {
+    // No weekend
+    return(false);
+  }
+  else {
+    // There is weekend
+    return(false);
+  }
+}
+function checkIfDayIsWeekend() {
+  // Check if it is weekend
+  var vertrek_datum = $("vertrek_datum").value;
+  var datum = new Date(vertrek_datum);
+  datum = datum.getDay();
+
+  if (datum == 5 || datum == 6 || datum == 0 || datum == 1) {
+    // Check if the day is in the weekend day
+    if (datum == 5) {
+      // If the weekend day is 5 or one we check the time that if that is also in the weekend time
+      return(weekendTime());
+    }
+    else if (datum == 1) {
+      return(weekendTime());
+    }
+    else {
+      return(true);
+    }
+  }
+  else {
+    // No weekend
+    return(false);
+  }
+}
+function minuutTime(time_in_hours) {
+  // calculates the time and price
+  var startTime = $("begin_tijd").value;
+  var endTime = $("eind_tijd").value;
+
+  startTime = startTime.replace(":", "");
+  endTime = endTime.replace(":", "");
+
+  if (startTime >= 800 && startTime <= 1800) {
+    // If the time is in the day time
+    return(time_in_hours * 0.25);
+  }
+  else {
+    // If it is night
+    return(time_in_hours * 0.45);
+  }
+}
+function drivePrice() {
+  // Cost per kilometer
+    var kilometers = $("kilometers").value;
+    kilometers = kilometers * 1;
+    return(kilometers);
+}
+
+function calculateCosts() {
+  // Calculate the cost
+  var time = calculateTravealing();
+  // The time we are on our way
+
+  var timePrice = minuutTime(time);
+  // Price per minuut to drive
+
+  var kilometers = getKilometers();
+  var drivePriceing = drivePrice(kilometers);
+  // Price per kilometer
+
+  var weekend = checkIfDayIsWeekend();
+
+  var price;
+
+  if (weekend == true) {
+    price = timePrice + drivePriceing;
+    price = price * 1.15;
+  }
+  else if (weekend == false) {
+    price = timePrice + drivePriceing;
+  }
+  displayPrice(price);
+}
+function calculateTravealing() {
+  // We calculate the time we are traffeling
+  var startTime = $("begin_tijd").value;
+  var endTime = $("eind_tijd").value;
+
+  startTime = startTime.replace(":", "");
+  endTime = endTime.replace(":", "");
+  // Converting the time to something we can calculate with
+
+  var traffleTime = endTime - startTime;
+
+  return(traffleTime);
+}
+function displayPrice(price) {
+  $("prijs").innerHTML = price;
+}
+function $(element) {
+  // Get a element by a ID
+  element = document.getElementById(element);
+  return(element);
+}
